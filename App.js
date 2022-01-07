cc3dad7c36dde50ac4d5c25b0499792d7fca0e75
AleksOmegaEngineering <ashar@aleksomega.com>
2022-01-07 16:25:25 -0600

    Search and filter frontend

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import * as Font from "expo-font";
 import {
+  Animated,
   ScrollView,
   StyleSheet,
   Text,
@@ -12,22 +13,16 @@ import {
   Image,
   AppRegistry,
   TouchableOpacity,
+  Easing,
+  TextInput,
+  TouchableWithoutFeedback,
+  Pressable,
+  Touchable
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FlatList } from "react-native-gesture-handler";
 
-let sampleName = [
-  "4040 Sierra Rd, Grand Prairie, TX 75052",
-  "4041 Sierra Rd, Grand Prairie, TX 75052",
-  "4042 Sierra Rd, Grand Prairie, TX 75052",
-];
-let sampleDescription = [
-  "3 bds 2 ba 1,239 sqft - House for sale",
-  "3 bds 2 ba 1,239 sqft - House for sale",
-  "3 bds 2 ba 1,239 sqft - House for sale",
-];
 const AOBlue = "#080B47";
 
 // async function fetchData(){
@@ -37,27 +32,257 @@ const AOBlue = "#080B47";
 //   return data;
 // }
 
-function Header(props) {
-  return (
-    <View style={styles.header}>
-      <View adjustsFontSizeToFit style={styles.imageContainer}>
-        <Image
-          style={styles.logo}
-          source={require("./assets/images/fullAOLogo.png")}
-        />
-      </View>
-      <Text adjustsFontSizeToFit numberOfLines={1} style={styles.headerText}>
-        ALEKS OMEGA
-      </Text>
-    </View>
+const RotateCwView = (props) => {
+  const spinValue = useRef(new Animated.Value(0)).current;
+  const fadeValue = useRef(new Animated.Value(0.2)).current;
+
+
+  React.useEffect(() =>{
+    Animated.loop(
+      Animated.parallel([
+        Animated.timing(spinValue, {
+            toValue: 1,
+            duration: 1000,
+            easing: Easing.linear,
+            useNativeDriver: false
+        }),
+        Animated.sequence([
+          Animated.timing(fadeValue, {
+              toValue: 1,
+              duration: 500,
+              easing: Easing.ease,
+              useNativeDriver: false
+          }),
+          Animated.timing(fadeValue, {
+            toValue: 0.2,
+            duration: 500,
+            easing: Easing.ease,
+            useNativeDriver: false
+        }),
+        ]) 
+      ])
+    ).start();
+  }, [spinValue, fadeValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg']
+  });
+
+  return(
+    <Animated.View
+      style={{
+        ...props.style,
+        transform:
+        [
+          {rotate: spin}
+        ],
+        opacity: fadeValue,
+      }}
+      >
+        {props.children}
+      </Animated.View>
+  );
+}
+
+// function Header(props) {
+//   const [text, onChangeText] = React.useState(null);
+//   return (
+//     <View style = {styles.headerBig}>
+//       <View style={styles.header}>
+//         <View adjustsFontSizeToFit style={styles.imageContainer}>
+//           <Image
+//             style={styles.logo}
+//             source={require("./assets/images/fullAOLogo.png")}
+//           />
+//         </View>
+//         <Text adjustsFontSizeToFit numberOfLines={1} style={styles.headerText}>
+//           ALEKS OMEGA
+//         </Text>
+//       </View>
+//       <View style = {styles.searchSortContainer}>
+//         <View style = {styles.searchContainer}>
+//           <Pressable style={styles.searchImageContainer}>
+//             <Image
+//             style={styles.searchImage}
+//             source={require("./assets/images/search.png")}
+//             />
+//           </Pressable>
+//           <TextInput
+//           style={styles.searchInput}
+//           onChangeText={onChangeText}
+//           value={text}
+//           />
+//         </View>
+//         <View style={styles.filterContainer}>
+//           <Image
+//           style={styles.filterImage}
+//           source={require("./assets/images/filter.png")}
+//           />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
+
+function Entries({item}){
+  return(
+      <TouchableOpacity
+        style={styles.fullEntry}
+        onPress={() => props.navigation.navigate("Information")}
+      >
+        <View style={styles.entrySample}>
+          <View style={styles.entryImageContainer}>
+            <Image
+              // source={require("./assets/images/sampleHouse.jpg")}
+              // source={this.state.image}
+              source={{
+                uri: `data:image/gif;base64,${item.profile_image}`,
+              }}
+              style={styles.entryImage}
+            />
+          </View>
+          <View style={styles.entryInformation}>
+            <View style={styles.entryNameCont}>
+              <Text
+                style={styles.entryName}
+                adjustsFontSizeToFit={true}
+                numberOfLines={2}
+              >
+                {item.name}
+              </Text>
+            </View>
+            <View style={styles.entryDescriptionCont}>
+              <Text
+                style={styles.entryDescription}
+                adjustsFontSizeToFit={true}
+                numberOfLines={2}
+              >
+                {item.description}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.seperator}></View>
+      </TouchableOpacity>
   );
 }
 
 export class HomeScreen extends React.Component {
-  state = {
-    data: [],
-    propertiesComponent: [],
-  };
+  constructor(props){
+    super(props)
+    this.state = {
+      data: [],
+      propertiesComponent: [],
+      image: null,
+      propertiesLoaded: false,
+      timer: false,
+      textInputValue: "",
+      useTextInput: false,
+    };
+  }
+  renderItem = ({itemObj}) =>{
+    <Entries item={itemObj}/>
+  }
+  searchListener = () =>{
+    console.log("WORLD");
+    const { textInputValue } = this.state;
+    console.log(this.state.textInputValue);
+  }
+  Header = (props) =>{
+    if(this.state.textInputValue == ""){
+      return (
+        <View style = {styles.headerBig}>
+          <View style={styles.header}>
+            <View adjustsFontSizeToFit style={styles.imageContainer}>
+              <Image
+                style={styles.logo}
+                source={require("./assets/images/fullAOLogo.png")}
+              />
+            </View>
+            <Text adjustsFontSizeToFit numberOfLines={1} style={styles.headerText} onPress={()=>{console.log("FUVK")}}>
+              ALEKS OMEGA
+            </Text>
+          </View>
+          <View style = {styles.searchSortContainer}>
+            <View style = {styles.searchContainer}>
+              <TouchableOpacity
+              style={styles.searchImageContainer}
+              onPress={this.searchListener}>
+                <Image
+                style={styles.searchImage}
+                source={require("./assets/images/search.png")}
+                />
+              </TouchableOpacity>
+              <View style={styles.searchInputContainer}>
+                <TextInput
+                style={styles.searchInput}
+                value={this.state.textInputValue}
+                onChangeText={textInputValue => this.setState({textInputValue})}
+                returnKeyType="done"
+                />
+              </View>
+            </View>
+            <View style={styles.filterContainer}>
+              <Image
+              style={styles.filterImage}
+              source={require("./assets/images/filter.png")}
+              />
+            </View>
+          </View>
+        </View>
+      );
+    }
+    else{
+      return (
+        <View style = {styles.headerBig}>
+          <View style={styles.header}>
+            <View adjustsFontSizeToFit style={styles.imageContainer}>
+              <Image
+                style={styles.logo}
+                source={require("./assets/images/fullAOLogo.png")}
+              />
+            </View>
+            <Text adjustsFontSizeToFit numberOfLines={1} style={styles.headerText} onPress={()=>{console.log("FUVK")}}>
+              ALEKS OMEGA
+            </Text>
+          </View>
+          <View style = {styles.searchSortContainer}>
+            <View style = {styles.searchContainer}>
+              <TouchableOpacity
+              style={styles.searchImageContainer}
+              onPress={this.searchListener}>
+                <Image
+                style={styles.searchImage}
+                source={require("./assets/images/search.png")}
+                />
+              </TouchableOpacity>
+              <View style={styles.searchInputContainer}>
+                <TextInput
+                style={styles.searchInput}
+                value={this.state.textInputValue}
+                onChangeText={textInputValue => this.setState({textInputValue})}
+                returnKeyType="done"
+                />
+              </View>
+              <View style={styles.closeContainer}>
+                <Image
+                style={styles.closeImage}
+                source={require("./assets/images/close.png")}
+                />
+              </View>
+            </View>
+            <View style={styles.filterContainer}>
+              <Image
+              style={styles.filterImage}
+              source={require("./assets/images/filter.png")}
+              />
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
   async fetchData() {
     let response = await fetch("http://192.168.1.139:4545/properties");
     let newData = await response.json();
@@ -65,60 +290,98 @@ export class HomeScreen extends React.Component {
     this.setState({ data: newData });
   }
   componentDidMount() {
-    this.fetchData().then(() =>{
-      properties = []
-      for (let i = 0; i < this.state.data.length; i++) {
-        properties.push(
-        <TouchableOpacity
-          style={styles.fullEntry}
-          onPress={() => this.props.navigation.navigate("Information")}
-          key={i}
-        >
-          <View style={styles.entrySample}>
-            <View style={styles.entryImageContainer}>
-              <Image
-                source={require("./assets/images/sampleHouse.jpg")}
-                style={styles.entryImage}
-              />
-            </View>
-            <View style={styles.entryInformation}>
-              <View style={styles.entryNameCont}>
-                <Text
-                  style={styles.entryName}
-                  adjustsFontSizeToFit={true}
-                  numberOfLines={2}
-                >
-                  {this.state.data[i][0]}
-                </Text>
+    setTimeout(()=>{
+      this.setState({timer: true});
+    }, 1500);
+    this.fetchData()
+      .then(() => {
+        properties = [];
+        for (let i = 0; i < this.state.data.length; i++) {
+          properties.push(
+            <TouchableOpacity
+              style={styles.fullEntry}
+              onPress={() => this.props.navigation.navigate("Information")}
+              key={i}
+            >
+              <View style={styles.entrySample}>
+                <View style={styles.entryImageContainer}>
+                  <Image
+                    source={{
+                      uri: `data:image/gif;base64,${this.state.data[i][3]}`,
+                    }}
+                    style={styles.entryImage}
+                  />
+                </View>
+                <View style={styles.entryInformation}>
+                  <View style={styles.entryNameCont}>
+                    <Text
+                      style={styles.entryName}
+                      adjustsFontSizeToFit={true}
+                      numberOfLines={2}
+                    >
+                      {this.state.data[i][1]}
+                    </Text>
+                  </View>
+                  <View style={styles.entryDescriptionCont}>
+                    <Text
+                      style={styles.entryDescription}
+                      adjustsFontSizeToFit={true}
+                      numberOfLines={2}
+                    >
+                      {this.state.data[i][2]}
+                    </Text>
+                  </View>
+                </View>
               </View>
-              <View style={styles.entryDescriptionCont}>
-                <Text
-                  style={styles.entryDescription}
-                  adjustsFontSizeToFit={true}
-                  numberOfLines={2}
-                >
-                  {this.state.data[i][1]}
-                </Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.seperator}></View>
-        </TouchableOpacity>
-        )
-      }
-      this.setState({propertiesComponent: properties});
-    });
+              <View style={styles.seperator}></View>
+            </TouchableOpacity>
+          );
+        }
+        this.setState({ propertiesComponent: properties });
+        this.setState({ propertiesLoaded: true});
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-  render(){
-    return(
-      <SafeAreaView style={styles.safeAreaView}>
+  render() {
+    console.log(this.state.textInputValue);
+    if(!(this.state.propertiesLoaded && this.state.timer)){
+      return (
+        <SafeAreaView style={styles.safeAreaView}>
         <StatusBar style={{ barStyle: "dark", backgroundColor: "#ffff" }} />
-        <Header />
+        <this.Header />
         <View style={styles.scrollArea}>
-          <ScrollView style={styles.scrollView}>{this.state.propertiesComponent}</ScrollView>
+          <ScrollView style={styles.scrollView}>
+            <RotateCwView style={styles.loadingLogoBigContainer}>
+                <Image
+                  style={styles.loadingLogo}
+                  source={require("./assets/images/AO.png")}
+                />
+            </RotateCwView >
+          </ScrollView>
         </View>
       </SafeAreaView>
-    );
+      );
+    }
+    else{
+      return(
+        <SafeAreaView style={styles.safeAreaView}>
+          <StatusBar style={{ barStyle: "dark", backgroundColor: "#ffff" }} />
+          <this.Header />
+          <View style={styles.scrollArea}>
+            <ScrollView style={styles.scrollView}>
+              {this.state.propertiesComponent}
+            </ScrollView>
+            {/* <FlatList
+              data={this.state.data}
+              renderItem={this.renderItem}
+              keyExtractor={item => item.id}
+            /> */}
+          </View>
+        </SafeAreaView>
+      );
+    }
   }
 }
 
@@ -139,7 +402,7 @@ function Navigation() {
           name="Home"
           component={HomeScreen}
         />
-        <Stack.Screen name="Information" component={Information} />
+        <Stack.Screen options={{ headerShown: false }} name="Information" component={Information} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -173,18 +436,29 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flexDirection: "row",
-    flex: 1,
+  headerBig: {
+    flexDirection: "column",
+    flex: 2,
     top: 0,
     backgroundColor: "#fff",
     borderColor: "#fff",
-    borderBottomColor: AOBlue,
-    borderWidth: 0,
-    borderBottomWidth: 40,
+    // borderColor: AOBlue,
+    // borderWidth: 5,
+    // borderBottomWidth: 40,
     justifyContent: "center",
     alignItems: "center",
-    paddingBottom: 5,
+    marginBottom: 5,
+  },
+  header: {
+    flexDirection: "row",
+    flex: 5,
+    width: "100%",
+    top: 0,
+    backgroundColor: "#fff",
+    // borderColor: "red",
+    // borderWidth: 5,
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     margin: 2,
@@ -192,6 +466,7 @@ const styles = StyleSheet.create({
     // borderWidth: 5,
     maxHeight: 67,
     width: 67,
+    marginLeft: 75
   },
   logo: {
     resizeMode: "contain",
@@ -207,19 +482,117 @@ const styles = StyleSheet.create({
     color: AOBlue,
     // borderColor: "red",
     // borderWidth: 5,
+    marginRight: 75,
+  },
+  searchSortContainer: {
+    flexDirection: "row",
+    flex: 2.5,
+    // borderColor: AOBlue,
+    // borderWidth: 5,
+    width: "100%",
+    backgroundColor: AOBlue,
+  },
+  searchContainer: {
+    marginLeft: "2%",
+    marginRight: "5%",
+    flexDirection: "row",
+    flex: 10,
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  searchImageContainer: {
+    height: "100%",
+    flex: 1,
+    // borderWidth: 5,
+    // borderColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  searchImage: {
+    height: "60%",
+    // borderColor: "white",
+    // borderWidth: 5,
+    maxWidth: "100%",
+    resizeMode: "contain"
+  },
+  searchInputContainer:{
+    height: "70%",
+    marginLeft: "2%",
+    // borderWidth: 3,
+    borderBottomWidth: 1,
+    borderColor: "white",
+    flex: 9,
+  },
+  searchInput:{
+    flex: 1,
+    color: "white",
+    fontSize: 16,
+    height: "100%",
+    width: "90%",
+    // borderColor: "white",
+    // borderWidth: 5,
+  },
+  closeContainer:{
+    position: "absolute",
+    // borderColor: "red",
+    // borderWidth: 1,
+    maxWidth: "7%",
+    height: "70%",
+    // flex: 1,
+    right: 0,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  closeImage: {
+    height: "100%",
+    maxWidth: "100%",
+    resizeMode: "contain"
+  },
+  filterContainer: {
+    marginRight: "2%",
+    height: "100%",
+    flex: 1,
+    // borderWidth: 5,
+    // borderColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  filterImage: {
+    height: "55%",
+    // borderColor: "white",
+    // borderWidth: 5,
+    maxWidth: "100%",
+    resizeMode: "contain"
   },
   safeAreaView: {
     flex: 1,
+    backgroundColor: "white",
   },
   scrollArea: {
     flex: 8,
+    backgroundColor: "white",
     // borderColor: "red",
     // borderWidth: 5,
   },
   scrollView: {
     flex: 1,
+  },
+  loadingLogoBigContainer: {
     // borderColor: "red",
     // borderWidth: 5,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingLogo: {
+    alignSelf: "center",
+    resizeMode: "contain",
+    // borderColor: "blue",
+    // borderWidth: 5,
+    maxHeight: "10%",
+    maxWidth: "10%",
   },
   fullEntry: {
     height: 100,
@@ -247,6 +620,7 @@ const styles = StyleSheet.create({
     // borderWidth: 5,
     justifyContent: "center",
     alignItems: "center",
+    marginLeft: "5%"
   },
   entryImage: {
     resizeMode: "contain",
